ede2ab1f243b348e862548be471622beafd4225d
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-06-27 11:00:40 +0100

    Rebuild each method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,14 @@
 module Enumerable
   # Your code goes here
+  def my_each
+    i = 0
+    until i == self.length do
+      yield(self[i])
+      i += 1
+    end
+
+    return self
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +17,7 @@ end
 # to this method
 class Array
   # Define my_each here
+  array = [1, 2, 3]
+
+  array.my_each { |elem| puts elem}
 end
